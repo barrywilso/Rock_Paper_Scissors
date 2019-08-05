3eb3df22e67670f66d5396326a19bf4eda2244aa
barrywilso <barrywilso@googlemail.com>
2019-08-05 17:50:24 +0100

    Adds game running

---
+++ controller.rb
@@ -0,0 +1,18 @@
+require('sinatra')
+require('sinatra/contrib/all') if development?
+require_relative('./models/game.rb')
+also_reload('./models/*')
+
+get '/' do
+  erb(:home)
+end
+
+get '/about' do
+erb(:about)
+end
+
+get '/play/:hand1/:hand2' do
+  game = Game.new(params[:hand1].to_s(), params[:hand2].to_s())
+  @outcome = game.play()
+  erb(:result)
+end
